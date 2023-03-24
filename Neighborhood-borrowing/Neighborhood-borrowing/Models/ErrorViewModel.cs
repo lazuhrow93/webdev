1005724a4c42928a25aaf5f480ab0e5ef2998bcb
Lazaro Hernandez (A) <lhernandez93@outlook.com>
2023-03-24 02:40:26 -0500

    adding new project

---
+++ Neighborhood-borrowing/Neighborhood-borrowing/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Neighborhood_borrowing.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
